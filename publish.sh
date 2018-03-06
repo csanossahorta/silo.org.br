3c0a35cbc8726ee62deb77909ee132d2ce6c2f6e
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-03-06 18:33:40 -0300

    small changes to publish script

--- publish.sh
+++ publish.sh
@@ -2,17 +2,18 @@
 
 git checkout --orphan gh-pages
 git rm --cached -r .
+
+jekyll b
 ls -la | grep -v "\(_site\|CNAME\|.git\|publish.sh\)" | xargs rm -rf
 
 cp -r _site/* .
-rm -rf _pages
+rm -rf _pages/ _site/
 
 git add .
-git commit -m "updates sites"
+git commit -m "updates site"
 git push origin :gh-pages
 git push -u origin gh-pages
 
-rm -rf _site
 git rm -rf *
 git checkout -f master
 git branch -D gh-pages
